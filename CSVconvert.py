92e5967eb99c6bc6533c35843ab9368a1063dd4a
Daniel <doldham21@massmutual.com>
2020-02-06 16:50:52 -0500

    Update explanation in CSVConvert python script

--- CSVconvert.py
+++ CSVconvert.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 Convert raw NOAA .txt files to .csv to easily import into Postgres
+
+Using fixed-width format with pd.read_fwf because of spaces in delims
+and in entries.
+
 """
 
 import pandas as pd
 
 # Stations data
-# Read based on fixed-width format
 stations = pd.read_fwf('/Users/mm19864/Documents/BU_assignments/CS779/TermProj/NOAA_BigData/data/text_files/ghcnd-stations.txt',
                 widths=[12,9,9,7,3,31,4,4,5], header=None)
 

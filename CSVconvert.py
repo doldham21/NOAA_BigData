be158afc72717ec8181ac82e0bdbd060954ae027
Daniel <doldham21@massmutual.com>
2020-02-06 16:43:36 -0500

    Build station info ingestion process, pre-process in Python

--- CSVconvert.py
+++ CSVconvert.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Convert raw NOAA .txt files to .csv to easily import into Postgres
 """
 
 import pandas as pd
 
 # Stations data
+# Read based on fixed-width format
 stations = pd.read_fwf('/Users/mm19864/Documents/BU_assignments/CS779/TermProj/NOAA_BigData/data/text_files/ghcnd-stations.txt',
-                widths=[12,10,9,10,31,4,4,5], header=None)
+                widths=[12,9,9,7,3,31,4,4,5], header=None)
 
+# Convert to csv for PG import
 stations.to_csv('/Users/mm19864/Documents/BU_assignments/CS779/TermProj/NOAA_BigData/data/ghcnd-stations.csv',
-                   sep=',', encoding='utf-8')
+                   sep=',', encoding='utf-8', header=None)
 
--- SQL/NOAA_buildDB.sql
+++ SQL/NOAA_buildDB.sql
@@ -3,7 +3,7 @@
 -- DROP TABLE STATIONS;
 -- Create table for ghcnd-stations.txt
 CREATE TABLE STATIONS (
-	StationNum CHAR(2) PRIMARY KEY NOT NULL,
+	StationNum VARCHAR(6) PRIMARY KEY NOT NULL,
 	StationID CHAR(11) NOT NULL,
 	Latitude NUMERIC NOT NULL,
 	Longtitude NUMERIC NOT NULL,
@@ -12,23 +12,20 @@ CREATE TABLE STATIONS (
 	StationName VARCHAR(30) NOT NULL,
 	GSN_Flag CHAR(3) NULL,
 	HCN_CRN_Flag CHAR(3) NULL,
-	WMO_ID VARCHAR(5) NULL);
+	WMO_ID NUMERIC NULL);
 	
 CREATE TABLE STATES (
 	StateNum INTEGER PRIMARY KEY NOT NULL,
 	StateAbbr CHAR(2) NOT NULL,
 	StateLong VARCHAR(50) NOT NULL);
 
--- CSV converstion in Python:
--- import pandas
--- stations = pandas.read_fwf('/Users/mm19864/Documents/BU_assignments/CS779/TermProj/NOAA_BigData/data/text_files/ghcnd-stations.txt',
---                widths=[11,8,8,6,30,3,3,5])
-
+-- CSV converstion in Python
 -- Import data from .csv file for station information, states, countries
-COPY STATIONS FROM '/Users/mm19864/Documents/BU_assignments/CS779/TermProj/NOAA_BigData/data/ghcnd-stations.csv' (FORMAT CSV, HEADER, DELIMITER(','));
-COPY STATES FROM '/Users/mm19864/Documents/BU_assignments/CS779/TermProj/NOAA_BigData/data/ghcnd-states.csv' (FORMAT CSV, HEADER, DELIMITER(','));
+COPY STATIONS FROM '/Users/mm19864/Documents/BU_assignments/CS779/TermProj/NOAA_BigData/data/ghcnd-stations.csv' (FORMAT CSV, DELIMITER(','));
+COPY STATES FROM '/Users/mm19864/Documents/BU_assignments/CS779/TermProj/NOAA_BigData/data/ghcnd-states.csv' (FORMAT CSV, DELIMITER(','));
 
 
 -- Check data was imported properly
 SELECT * FROM STATIONS
+LIMIT 5;
 WHERE stationstate = 'AK'

06a85d5c1addb075e4f820e5087a65145bb50803
mathemacode <43391344+mathemacode@users.noreply.github.com>
2020-02-13 20:40:18 -0500

    Update paths

--- Python/CSVconvert.py
+++ Python/CSVconvert.py
@@ -11,34 +11,34 @@ import pandas as pd
 import glob
 
 # Stations data
-stations = pd.read_fwf('/Users/mm19864/Documents/BU_assignments/CS779/TermProj/NOAA_BigData/data/text_files/ghcnd-stations.txt',
+stations = pd.read_fwf('C:/Users/dell/Documents/GitHub/NOAA_BigData/data/text_files/ghcnd-stations.txt',
                 widths=[12,9,9,7,3,31,4,4,5], header=None)
 
 # Convert to csv for PG import
-stations.to_csv('/Users/mm19864/Documents/BU_assignments/CS779/TermProj/NOAA_BigData/data/ghcnd-stations.csv',
+stations.to_csv('C:/Users/dell/Documents/GitHub/NOAA_BigData/data/ghcnd-stations.csv',
                    sep=',', encoding='utf-8', header=None)
 
 
 # States data
-states = pd.read_fwf('/Users/mm19864/Documents/BU_assignments/CS779/TermProj/NOAA_BigData/data/text_files/ghcnd-states.txt',
+states = pd.read_fwf('C:/Users/dell/Documents/GitHub/NOAA_BigData/data/text_files/ghcnd-states.txt',
                 widths=[3,35], header=None)
 
 # Convert to csv for PG import
-states.to_csv('/Users/mm19864/Documents/BU_assignments/CS779/TermProj/NOAA_BigData/data/ghcnd-states.csv',
+states.to_csv('C:/Users/dell/Documents/GitHub/NOAA_BigData/data/ghcnd-states.csv',
                    sep=',', encoding='utf-8', header=None)
 
 
 # Countries data
-countries = pd.read_fwf('/Users/mm19864/Documents/BU_assignments/CS779/TermProj/NOAA_BigData/data/text_files/ghcnd-countries.txt',
+countries = pd.read_fwf('C:/Users/dell/Documents/GitHub/NOAA_BigData/data/text_files/ghcnd-countries.txt',
                 widths=[3,200], header=None)
 
 # Convert to csv for PG import
-countries.to_csv('/Users/mm19864/Documents/BU_assignments/CS779/TermProj/NOAA_BigData/data/ghcnd-countries.csv',
+countries.to_csv('C:/Users/dell/Documents/GitHub/NOAA_BigData/data/ghcnd-countries.csv',
                    sep=',', encoding='utf-8', header=None)
 
 
 # Inventory data
-countries = pd.read_fwf('/Users/mm19864/Documents/BU_assignments/CS779/TermProj/NOAA_BigData/data/text_files/ghcnd-inventory.txt',
+countries = pd.read_fwf('C:/Users/dell/Documents/GitHub/NOAA_BigData/data/text_files/ghcnd-inventory.txt',
                 widths=[12,9,9,5,5,5], header=None)
 
 # Convert to csv for PG import
@@ -49,9 +49,9 @@ countries.to_csv('/Users/mm19864/Documents/BU_assignments/CS779/TermProj/NOAA_Bi
 
 # Concat all .dly files in directory together
 # Credit: https://stackoverflow.com/questions/17749058/
-read_files = glob.glob("C:/Users/dell/Documents/ghcnd_all/ghcnd_all/*.dly")
+read_files = glob.glob("D:/ghcnd_all/ghcnd_all/*.dly")
 
-with open("C:/Users/dell/Documents/result.txt", "wb") as outfile:
+with open("D:/result.txt", "wb") as outfile:
     for f in read_files:
         with open(f, "rb") as infile:
             outfile.write(infile.read())
@@ -60,12 +60,12 @@ with open("C:/Users/dell/Documents/result.txt", "wb") as outfile:
 # Credit: https://stackoverflow.com/questions/45870220
 
 # For each chunk of data, convert to CSV and append into single file 'ghcnd-all.csv'
-for chunk in pd.read_fwf('C:/Users/dell/Documents/result.txt',
+for chunk in pd.read_fwf('D:/result.txt',
                 widths=[11,4,2,4,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1
                         ,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1
                         ,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1],
                         header=None, chunksize=1500):
     
-    chunk.to_csv('C:/Users/dell/Documents/GitHub/NOAA_BigData/data/ghcnd-all.csv',
+    chunk.to_csv('D:/ghcnd-all.csv',
                        mode='a', sep=',', encoding='utf-8', header=None)
 

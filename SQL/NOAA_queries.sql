ce61b178dd0fc01defec2e216be4c9161c9c7db2
Daniel <doldham21@massmutual.com>
2020-02-13 19:37:59 -0500

    Add initial query test

--- SQL/NOAA_queries.sql
+++ SQL/NOAA_queries.sql
@@ -1,1 +1,10 @@
 -- Query database as desired
+
+-- Join Stations, Countries, States
+SELECT obsnum, stationid, stationname, year, month, element, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, StateLong, CountryLong 
+FROM OBS
+JOIN Stations USING(StationID)
+JOIN States USING(StateAbbr)
+JOIN Countries USING(CountryAbbr)
+--WHERE Element = 'TMAX' OR Element = 'TMIN'
+ORDER BY obsnum;

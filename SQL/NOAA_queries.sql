57b819a3c4365d3dd31bc648492d8562a7b4d54e
mathemacode <43391344+mathemacode@users.noreply.github.com>
2020-02-14 13:16:55 -0500

    Add query for local (statewide) info

--- SQL/NOAA_queries.sql
+++ SQL/NOAA_queries.sql
@@ -6,5 +6,17 @@ FROM OBS
 JOIN Stations USING(StationID)
 JOIN States USING(StateAbbr)
 JOIN Countries USING(CountryAbbr)
---WHERE Element = 'TMAX' OR Element = 'TMIN'
-ORDER BY obsnum;
+WHERE Element = 'TMAX' OR Element = 'TMIN'
+ORDER BY obsnum
+LIMIT 10000;
+
+
+-- Find TMAX and TMIN information for certain state
+SELECT obsnum, stationid, stationname, year, month, element, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, StateAbbr, StateLong, CountryLong 
+FROM OBS
+JOIN Stations USING(StationID)
+JOIN States USING(StateAbbr)
+JOIN Countries USING(CountryAbbr)
+WHERE Element IN('TMAX','TMIN') AND StateAbbr = 'FL'
+ORDER BY year DESC
+LIMIT 2000;

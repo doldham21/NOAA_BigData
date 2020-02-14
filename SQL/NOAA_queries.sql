96c3629dffe4a15e53ae9825087e088c4aaf817c
mathemacode <43391344+mathemacode@users.noreply.github.com>
2020-02-14 17:16:08 -0500

    Add queries

--- SQL/NOAA_queries.sql
+++ SQL/NOAA_queries.sql
@@ -1,5 +1,17 @@
 -- Query database as desired
 
+-- Find full data 
+SELECT *, SF1 FROM INVENTORY
+JOIN OBS USING(StationID, Element)
+WHERE endyear = 2020 AND SF2 IS NULL
+AND INVENTORY.Element = 'TMAX'
+LIMIT 100;
+
+-- Station data
+SELECT * FROM OBS
+WHERE StationID = 'AEM00041194'
+AND Element = 'TMAX';
+
 -- Join Stations, Countries, States
 SELECT obsnum, stationid, stationname, year, month, element, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, StateLong, CountryLong 
 FROM OBS
@@ -21,11 +33,58 @@ WHERE Element IN('TMAX','TMIN') AND StateAbbr = 'FL'
 ORDER BY year DESC
 LIMIT 2000;
 
--- Find average max temp
-SELECT obsnum, stationid, stationname, year, month, element, ((v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18+v19+v20+v21+v22+v23+v24+v25+v26+v27+v28+v29+v30+v31)/31)/10 AS AVG_HIGH, StateAbbr, StateLong, CountryLong 
+-- Find average max temp for a certain state (and month / station if desired)
+SELECT obsnum, stationid, stationname, year, month, element,
+((v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18+v19+v20+v21+v22+v23+v24+v25+v26+v27+v28+v29+v30+v31)/31)/10 AS Month_AVG, StateAbbr, StateLong, CountryLong 
 FROM obs
 JOIN Stations USING(StationID)
 JOIN States USING(StateAbbr)
 JOIN Countries USING(CountryAbbr)
-WHERE Element IN('TMAX') AND StateAbbr = 'FL' AND ((v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18+v19+v20+v21+v22+v23+v24+v25+v26+v27+v28+v29+v30+v31)/31)/10 IS NOT NULL
-ORDER BY year DESC;
+WHERE Element IN('TMAX')
+	--AND StationID = 'AGM00060457'
+	AND month = 1
+	AND StateAbbr = 'FL'
+ORDER BY year DESC, month;
+
+-- For non-null variables in v#, add together and average based on how many there are?  How to attempt this?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

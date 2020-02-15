f44a9fd69118a8bbdc670b253f3e428a98a8bf83
mathemacode <43391344+mathemacode@users.noreply.github.com>
2020-02-14 20:42:50 -0500

    Refactor

--- SQL/NOAA_buildAVERAGES.sql
+++ SQL/NOAA_buildAVERAGES.sql
@@ -1,6 +1,5 @@
 -- Create average columns for full months where data is available
-
--- Find average MAX TEMP for a certain state depending on month (30 or 31 days - ignoring february for now)
+-- Find average ELEMENT for certain year from a certain place (state or country)
 
 WITH 
 average_CTE AS 
@@ -9,10 +8,10 @@ average_CTE AS
 -- 31 day months
 (
 SELECT stationid, stationname, stateabbr, month, year,
-((v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18+v19+v20+v21+v22+v23+v24+v25+v26+v27+v28+v29+v30+v31)/31) AS TMAX_Month_AVG_C
+((v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18+v19+v20+v21+v22+v23+v24+v25+v26+v27+v28+v29+v30+v31)/31) AS month_avg
 FROM obs
---INNER JOIN Stations USING(StationID)
-WHERE Element IN('TMAX')
+INNER JOIN Stations USING(StationID)
+WHERE Element IN('TMIN')
 	AND ((v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18+v19+v20+v21+v22+v23+v24+v25+v26+v27+v28+v29+v30+v31)/31) IS NOT NULL
 	AND month IN(1,3,5,7,8,10,12)
 	--AND StateAbbr = 'FL'
@@ -24,10 +23,10 @@ UNION ALL
 -- 30 day months
 (
 SELECT stationid, stationname, stateabbr, month, year,
-((v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18+v19+v20+v21+v22+v23+v24+v25+v26+v27+v28+v29+v30)/30) AS TMAX_Month_AVG_C
+((v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18+v19+v20+v21+v22+v23+v24+v25+v26+v27+v28+v29+v30)/30) AS month_avg
 FROM obs
---INNER JOIN Stations USING(StationID)
-WHERE Element IN('TMAX')
+INNER JOIN Stations USING(StationID)
+WHERE Element IN('TMIN')
 	AND ((v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18+v19+v20+v21+v22+v23+v24+v25+v26+v27+v28+v29+v30)/30) IS NOT NULL
 	AND month IN(4,6,9,11)
 	--AND StateAbbr = 'FL'
@@ -39,10 +38,10 @@ UNION ALL
 -- February, 28 days, NOT leap years
 (
 SELECT stationid, stationname, stateabbr, month, year,
-((v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18+v19+v20+v21+v22+v23+v24+v25+v26+v27+v28)/28) AS TMAX_Month_AVG_C
+((v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18+v19+v20+v21+v22+v23+v24+v25+v26+v27+v28)/28) AS month_avg
 FROM obs
---INNER JOIN Stations USING(StationID)
-WHERE Element IN('TMAX')
+INNER JOIN Stations USING(StationID)
+WHERE Element IN('TMIN')
 	AND ((v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18+v19+v20+v21+v22+v23+v24+v25+v26+v27+v28)/28) IS NOT NULL
 	AND month IN(2)
 	AND year NOT IN(1904, 1908, 1912, 1916, 1920, 1924, 1928, 1932, 
@@ -58,10 +57,10 @@ UNION ALL
 -- February, 29 days, leap years
 (
 SELECT stationid, stationname, stateabbr, month, year,
-((v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18+v19+v20+v21+v22+v23+v24+v25+v26+v27+v28)/29) AS TMAX_Month_AVG_C
+((v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18+v19+v20+v21+v22+v23+v24+v25+v26+v27+v28)/29) AS month_avg
 FROM obs
---INNER JOIN Stations USING(StationID)
-WHERE Element IN('TMAX')
+INNER JOIN Stations USING(StationID)
+WHERE Element IN('TMIN')
 	AND ((v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11+v12+v13+v14+v15+v16+v17+v18+v19+v20+v21+v22+v23+v24+v25+v26+v27+v28)/29) IS NOT NULL
 	AND month IN(2)
 	AND year IN(1904, 1908, 1912, 1916, 1920, 1924, 1928, 1932, 
@@ -72,38 +71,34 @@ WHERE Element IN('TMAX')
 	AND CountryAbbr = 'US'
 )
 
---ORDER BY stationid, year, month
 	
 ),
 
-
--- Note that as of making this script 2020 just started, so only using past data
 
 permonth_CTE AS
 (
 -- Averages per month
 SELECT month, year, COUNT(DISTINCT(month)) AS nummonths, 
-		AVG(tmax_month_avg_c) AS tmax_average_monthly
+		AVG(month_avg) AS average_monthly
 FROM average_CTE
 GROUP BY month, year
---ORDER BY year, month
 ),
+
 
 peryear_CTE AS
 (
 -- Per year averages, all stations
 -- Logic is correct: isolating one year to one month in permonth_CTE
 -- lets me count the number of times that year appears as the number of months
-SELECT year, AVG(tmax_average_monthly) AS tmax_average_yearly, 
+SELECT year, AVG(average_monthly) AS average_yearly, 
 		COUNT(year) AS num_months
 FROM permonth_CTE
 GROUP BY year
---ORDER BY year
 )
 
 -- Only use data that has a full 12-month average
 -- Convert to C, not tenths of C
-SELECT year, (tmax_average_yearly/10) AS tmax_yearly FROM peryear_CTE
+SELECT year, (average_yearly/10) AS avg_yearly FROM peryear_CTE
 WHERE num_months = 12
 ORDER BY year;
 

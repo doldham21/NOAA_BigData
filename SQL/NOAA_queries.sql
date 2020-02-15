eeaec07c6aaa6ffa7f04d36acd46d201ba29b4c2
mathemacode <43391344+mathemacode@users.noreply.github.com>
2020-02-14 20:07:34 -0500

    Add CountryAbbr directly to OBS, update queries

--- SQL/NOAA_queries.sql
+++ SQL/NOAA_queries.sql
@@ -47,19 +47,6 @@ WHERE Element IN('TMAX')
 	AND StateAbbr = 'CA'
 ORDER BY year DESC, month;
 
--- For non-null variables in v#, add together and average based on how many there are?  How to attempt this?
-FOR VAL IN (v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15,v16,v17,v18,v19,v20,v21,v22,v23,v24,v25,v26,v27,v28,v29,v30,v31)
-
-WHILE VAL IS NOT NULL LOOP
-	counter := counter + 1;
-	summ := summ + val;
-	
-END LOOP;
-
-AVG_non_nulls = summ / counter
-
-
-
 
 
 
